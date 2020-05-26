2c1a7742f1e5e75f57cda8fe0e5623a334b99915
Charlie <ctslone27@gmail.com>
2020-05-26 19:39:46 -0400

    adding reactstrap to the project. trying to decide how to comment widget should look.

--- src/Components/Widget/index.js
+++ src/Components/Widget/index.js
@@ -1,6 +1,8 @@
 import React from "react";
 import "./widget-style.css";
 
+import { Button, Form, FormGroup, Label, Input, FormText, Container } from 'reactstrap';
+
 const Widget = (props) => {
     return (
         <div class="container">
@@ -15,6 +17,26 @@ const Widget = (props) => {
                 </fieldset>
             </form>
         </div>
+        // <Container>
+        //     <Form>
+        //         <FormGroup>
+        //             <Label for="exampleEmail">Email</Label>
+        //             <Input type="email" name="email" id="exampleEmail" placeholder="with a placeholder" />
+        //         </FormGroup>
+        //         <FormGroup>
+        //             <Label for="examplePassword">Password</Label>
+        //             <Input type="password" name="password" id="examplePassword" placeholder="password placeholder" />
+        //         </FormGroup>
+
+
+        //         <FormGroup>
+        //             <Label for="exampleText">Text Area</Label>
+        //             <Input type="textarea" name="text" id="exampleText" />
+        //         </FormGroup>
+
+        //         <Button>Submit</Button>
+        //     </Form>
+        // </Container>
     )
 }
 
