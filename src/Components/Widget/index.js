4c0063301a79c03787b3dc81faef1dc02d6ce8c6
Charlie <ctslone27@gmail.com>
2020-06-03 19:59:57 -0400

    adding some styles to background color, border and font style.

--- src/Components/Widget/index.js
+++ src/Components/Widget/index.js
@@ -9,10 +9,10 @@ const Widget = (props) => {
     return (
 
         <Container>
-            <div className="row justify-content-center">
-                <div className="card">
-                    <div className="logo-container">
-                        <img src="../../../media/logo.png" className="card-img-top main-logo" alt="..." />
+            <div className="row justify-content-center widget">
+                <div className="card col-10 widget__text">
+                    <div className="widget__image-box">
+                        <img src="../../../media/logo.png" className="card-img-top widget__image-box--logo" alt="..." />
                     </div>
                     <div className="card-body">
                         <p className="card-text">Hey there! During this uncertain time we would love to hear about what is working and what doesnt really work well.</p>
@@ -20,7 +20,9 @@ const Widget = (props) => {
                         <div>Seven Oaks</div>
 
                         <div class="form-group">
+                        <hr></hr>
                             <label for="exampleFormControlTextarea1">Please enter your comments and suggestions below and click submit.</label>
+                            
                             <textarea class="form-control" id="exampleFormControlTextarea1" rows="3"></textarea>
                         </div>
 
