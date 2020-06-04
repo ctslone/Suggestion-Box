7ea390ef43844952b50190639857bc8f3069e558
Charlie <ctslone27@gmail.com>
2020-06-03 22:08:47 -0400

    got the handle submit to access state and console long the value in the textarea.

--- src/App.js
+++ src/App.js
@@ -4,31 +4,29 @@ import './App.css';
 import Widget from './Components/Widget';
 
 class App extends Component {
-
-  // constructor(props) {
-  //   super(props);
-  //   this.state = {value: ''};
-
-  //   this.handleChange = this.handleChange.bind(this);
-  //   this.handleSubmit = this.handleSubmit.bind(this);
-  // }
-  state = {
-    comment: '',
-    stuff: "hey"
+  constructor(props) {
+    super(props)
+    this.state = { 
+      comment: '',
+      stuff: "hey"}
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this)
   }
+
+  // state = {
+  //   comment: '',
+  //   stuff: "hey"
+  // }
 
   handleChange = event => {
     console.log(event.target.value)
     this.setState({
       comment: event.target.value
     });
-    
-    // console.log("working")
   }
 
   handleSubmit() {
-    let words = this.state.comment
-    console.log(words);
+    console.log(this.state.comment)
   }
 
   render() {
